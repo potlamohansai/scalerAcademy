46cd71fa29fd099e0ccdd7c3113f390a9a34a203
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-03 22:16:09 +0530

    Create Q5manualPatternPrinting1.java

---
+++ Module02/Day04/Q5manualPatternPrinting1.java
@@ -0,0 +1,28 @@
+/*
+Print the following pattern in output
+===============
+*****  
+****  
+***  
+**  
+*
+=========
+*/
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        for(int i=5; i>0; i--){
+            for(int j=i; j>0; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
