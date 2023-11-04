9d20e97aa47e6647bbe6fc7356110cd34112dabd
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-04 20:47:51 +0530

    Create Q9printABCDE.java

---
+++ Module02/Day04/Q9printABCDE.java
@@ -0,0 +1,20 @@
+/*
+Print the first five letters of the English alphabet i.e. A, B, C, D and E.
+*/
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+  }
+}
