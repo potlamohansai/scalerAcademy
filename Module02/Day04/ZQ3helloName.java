b93c1280e96059a575d767a30446909b79839272
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-04 21:05:04 +0530

    Create ZQ3helloName.java

---
+++ Module02/Day04/ZQ3helloName.java
@@ -0,0 +1,18 @@
+/*
+Given a name A as input. Print "Hello A", where A is the name in input.
+  */
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hello "+name);
+    }
+}
