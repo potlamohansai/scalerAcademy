e24d05649fc914ef892043bab78b8b0842585ecd
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-04 21:03:47 +0530

    Create ZQ2printSentence.java

---
+++ Module02/Day04/ZQ2printSentence.java
@@ -0,0 +1,18 @@
+/*
+Given an english sentence, take it in the input and print it in the output.
+  */
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        System.out.println(n);
+    }
+}
