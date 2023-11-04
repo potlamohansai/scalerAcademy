ce7965830a10e1fc77ec91f070567d6463d3b723
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-04 20:45:52 +0530

    Create Q7simpleMaths.java

---
+++ Module02/Day04/Q7simpleMaths.java
@@ -0,0 +1,23 @@
+/*
+Perform the following operations on 6 and 3 and print their respective outputs in different lines.
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+  */
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        System.out.println(6+3);
+        System.out.println(6-3);
+        System.out.println(6*3);
+        System.out.println(6/3);
+    }
+}
