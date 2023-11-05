222ea3b6aac414bfbeaf8a87f8752e794b58117c
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-05 08:01:20 +0530

    Create ZQ8floorOfAbyB.java

---
+++ Module02/Day05/ZQ8floorOfAbyB.java
@@ -0,0 +1,21 @@
+/*
+Given two numbers A and B. Print the floor of A/B.
+  */
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc= new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A/B);
+    }
+}
