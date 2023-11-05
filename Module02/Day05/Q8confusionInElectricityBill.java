53992e509d4200a6b65af2484406036ab8f13a9b
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-05 07:50:03 +0530

    Update and rename Confusion in Electricity Bill to Q8confusionInElectricityBill.java

--- Module02/Day05/Q8confusionInElectricityBill.java
+++ Module02/Day05/Q8confusionInElectricityBill.java
@@ -1,3 +1,21 @@
+/*
+Mr. T got the Electricity bill of his house. He felt that the bill amount was too much. He come to you to understand the relation between amount and number of units with Electricity bill.
+
+
+Instructions are give on Electricity biil that :
+1. For first 50 units Rs. 0.50/unit.
+2. For next 100 units Rs. 0.75/unit.
+3. For next 100 units Rs. 1.20/unit.
+4. For above 250 units Rs. 1.50/unit.
+5. An additional surcharge of 20% is added to the bill.
+
+NOTE: As the electricity bill can have any real value (floating point), you have to tell the Integral value of the bill. For eg. Integral value of 2.91 is 2.
+
+To avoid manual calculation again and again, You have to write a code which take number of Unit (suppose N) from user and print the amount.
+    */
+
+
+
 import java.lang.*;
 import java.util.*;
 
