f029a9784d3a227ec4763c55e94652392ef2280d
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-06 07:40:25 +0530

    Update and rename PalindromicInteger.java to ZQ6palindromicInteger.java

--- Module02/Day06/ZQ6palindromicInteger.java
+++ Module02/Day06/ZQ6palindromicInteger.java
@@ -1,3 +1,12 @@
+/*
+You are given an integer A as input and you need to determine whether it is a palindrome or not.
+A palindrome integer is one whose digits, when reversed, result in the same number.
+For example, 121 is a palindrome because its reverse is also 121, but 123 is not a palindrome because its reverse is 321.
+
+Note: The given integer will not have any leading zeros.
+    */
+
+
 import java.lang.*;
 import java.util.*;
 
