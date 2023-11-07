ca32401263c9f9e47e7d12869452b4c019099853
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-07 20:36:39 +0530

    Update and rename ArmstrongNumbers.java to ZQ3armstrongNumbers.java

--- Module02/Day07/ZQ3armstrongNumbers.java
+++ Module02/Day07/ZQ3armstrongNumbers.java
@@ -1,3 +1,21 @@
+/*
+Problem Description
+You are given an integer N you need to print all the Armstrong Numbers between 1 to N. (N inclusive).
+
+If sum of cubes of each digit of the number is equal to the number itself, then the number is called an Armstrong number.
+
+For example, 153 = ( 1 * 1 * 1 ) + ( 5 * 5 * 5 ) + ( 3 * 3 * 3 ).
+
+Note: All the test cases in this problem are limited to 3 digit numbers.
+
+
+
+Problem Constraints
+1 <= N <= 500
+
+    */
+
+
 import java.lang.*;
 import java.util.*;
 
