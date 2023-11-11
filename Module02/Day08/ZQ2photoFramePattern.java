877c539600517b8f4bd80ccf42fee021beb5c1d8
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-11 15:40:07 +0530

    Update and rename PhotoFramePattern.java to ZQ2photoFramePattern.java

--- Module02/Day08/ZQ2photoFramePattern.java
+++ Module02/Day08/ZQ2photoFramePattern.java
@@ -1,3 +1,22 @@
+/*
+Problem Description
+Take an integer N as input, print the corresponding pattern for N.
+
+For example if N = 5 then pattern will be like:
+
+*****
+*   *
+*   *
+*   *
+*****
+Note : Except for the first line and the last line, every line will have N-2 spaces between the two stars.
+
+
+Problem Constraints
+3 <= N <= 100
+    */
+
+
 import java.lang.*;
 import java.util.*;
 
