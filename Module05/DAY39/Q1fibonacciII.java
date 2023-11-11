0ee47804c2ced14f23d963a7b724de349da89ae4
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-11 15:26:13 +0530

    Create Q1fibonacciII.java

---
+++ Module05/DAY39/Q1fibonacciII.java
@@ -0,0 +1,25 @@
+/* The Fibonacci numbers are the numbers in the following integer sequence.
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+
+In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation:
+
+Fn = Fn-1 + Fn-2
+
+Given a number A, find and return the Ath Fibonacci Number using recursion.
+
+Given that F0 = 0 and F1 = 1.
+
+Problem Constraints
+0 <= A <= 20
+
+*/
+
+public class Solution {
+    public int findAthFibonacci(int A) {
+         if (A == 0 || A==1){
+             return A;
+         }
+        return findAthFibonacci(A - 1) + findAthFibonacci(A - 2); 
+    }
+}
