f76a8167ec7ee6855621a0e521c3576a9bfe5573
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-13 20:38:52 +0530

    Update and rename FrequencyofXinArray.java to Q7frequencyofXinArray.java

--- Module02/Day10/Q7frequencyofXinArray.java
+++ Module02/Day10/Q7frequencyofXinArray.java
@@ -1,3 +1,14 @@
+/*Problem Description
+Given an integer array A. Find and return the frequency of number B in array A.
+
+
+Problem Constraints
+1 <= |A| <= 105
+1 <= A[i] <= 109
+1 <= B <= 109
+    */
+
+
 public class Solution {
     public int solve(int[] A, int B) {
         int len = A.length;
