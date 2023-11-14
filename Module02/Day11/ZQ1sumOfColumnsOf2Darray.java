9b060173c89cce44061596c56f30a72810fa7ed3
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-14 17:50:57 +0530

    Update and rename sumOfColumns.java to ZQ1sumOfColumnsOf2Darray.java

--- Module02/Day11/ZQ1sumOfColumnsOf2Darray.java
+++ Module02/Day11/ZQ1sumOfColumnsOf2Darray.java
@@ -1,3 +1,14 @@
+/*
+Problem Description
+Given a 2D integer array C[][] of A rows and B columns. Return an integer array of size B that represents the sums of the columns of the 2D array C.
+
+
+Problem Constraints
+1 <= A,B <= 103
+1 <= C[i][j] <= 103
+    */
+
+
 public class Solution {
     public ArrayList<Integer> solve(int A, int B, ArrayList<ArrayList<Integer>> C) {
         ArrayList<Integer> ans = new ArrayList<Integer>();
