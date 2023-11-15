89ce288896944c66f56f7e20adae69bcedbd3a2a
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-15 19:24:23 +0530

    Update and rename frequencyOfElementQuery.java to ZQ1frequencyOfElementQuery.java

--- Module02/Day14/ZQ1frequencyOfElementQuery.java
+++ Module02/Day14/ZQ1frequencyOfElementQuery.java
@@ -1,3 +1,17 @@
+/*
+Problem Description
+Given an array A. You have some integers given in the array B.
+For the i-th number, find the frequency of B[i] in the array A and return a list containing all the frequencies.
+
+
+Problem Constraints
+1 <= |A| <= 105
+1 <= |B| <= 105
+1 <= A[i] <= 105
+1 <= B[i] <= 105
+    */
+
+
 public class Solution {
     public ArrayList<Integer> solve(ArrayList<Integer> A, ArrayList<Integer> B) {
 
