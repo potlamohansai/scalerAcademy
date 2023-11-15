7dd50ad7d2592647cdd7f1a3ebe41c2637bd217b
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-15 19:09:12 +0530

    Update and rename palindromeorNot.java to Q6palindromeOrNot.java

--- Module02/Day13/Q6palindromeOrNot.java
+++ Module02/Day13/Q6palindromeOrNot.java
@@ -1,3 +1,11 @@
+/*Problem Description:
+
+Write a program to check whether the given string is a palindrome or not. Return True if it is palindrome else return False.
+
+Note: A string is said to be a palindrome if the reverse of the string is the same as the string itself.
+    */
+
+
 public static boolean isPalin(String s) {
     // complete the function template
     int len = s.length();
