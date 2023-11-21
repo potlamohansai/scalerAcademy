f9b1748a3e76e9fc636e911efe4a84058bc742e3
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-21 21:05:30 +0530

    Update and rename isPrime.java to Q2isPrime.java

---
+++ Module03/Day17/Q2isPrime.java
@@ -0,0 +1,27 @@
+/*Given a number A. Return 1 if A is prime and return 0 if not. 
+
+Note : 
+The value of A can cross the range of Integer.
+
+
+Problem Constraints
+1 <= A <= 109
+    */
+
+
+//Time Complexity O(sqrt(N))
+//Space Complexity O(1)
+
+public class Solution {
+    public int solve(int A) {
+        int count=0;
+        for(int i=1; i*i<=A; i++){
+            if(A%i ==0){
+                if(i == A/i) count++;
+                else count +=2;
+            }
+        }
+        if(count==2) return 1;
+        return 0;
+    }
+}
