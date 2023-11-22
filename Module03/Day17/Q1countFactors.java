c0317a20d9187fd0e3d95f9fb80ec3cd7f0f9bdf
Mohan Sai Potla <potlamohansai@gmail.com>
2023-11-22 20:47:14 +0530

    Update Q1countFactors.java

--- Module03/Day17/Q1countFactors.java
+++ Module03/Day17/Q1countFactors.java
@@ -1,23 +1,20 @@
-/*
-Problem Description
+/*Problem Description
 Given an integer A, you need to find the count of it's factors.
-
 Factor of a number is the number which divides it perfectly leaving no remainder.
 
 Example : 1, 2, 3, 6 are factors of 6
 
-
 Problem Constraints
-1 <= A <= 109
+1 <= A <= 10^9
 */
 
-//Time Complexity O(sqrt(n))
+//Time Complexity O(sqrt(A))
 //Space Complexity O(1)
 
 public class Solution {
     public int solve(int A) {
         int count =0;
-        for(int i=1; i*i <=A; i++){
+        for(int i=1; i*i <=A; i++){                //iterating from 1 to sqrt(A)
             if(A%i ==0){
                 if(i == A/i) count++;
                 else count +=2;
